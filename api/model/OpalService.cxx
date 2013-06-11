0902cc00de237c01883066e628b3b15ee387ecbf
Patrick Emond <emondpd@mcmaster.ca>
2013-06-11 17:47:32 -0400

    Integrating Opal directly using the curl library

--- api/model/OpalService.cxx
+++ api/model/OpalService.cxx
@@ -18,6 +18,9 @@
 
 #include <sstream>
 #include <stdexcept>
+#include </usr/include/curl/curl.h>
+#include </usr/include/curl/stdcheaders.h>
+#include </usr/include/curl/easy.h>
 
 namespace Alder
 {
@@ -42,21 +45,64 @@ namespace Alder
   }
 
   //-+#+-+#+-+#+-+#+-+#+-+#+-+#+-+#+-+#+-+#+-+#+-+#+-+#+-+#+-+#+-+#+-+#+-+#+-
-  Json::Value OpalService::Read( std::string servicePath )
+  Json::Value OpalService::Read( std::string servicePath, std::string fileName )
   {
+    bool toFile = 0 < fileName.length();
+    FILE *file;
+    CURL *curl;
+    std::stringstream urlStream;
+    std::string credentials, url, result;
+    struct curl_slist *headers = NULL;
+    CURLcode res;
     Json::Value root;
     Json::Reader reader;
-    
-    std::stringstream stream;
-    stream << "./opal.py --opal https://" << this->Host << ":" << this->Port
-           << " --user " << this->Username 
-           << " --password " << this->Password
-           << " --ws \"" << servicePath << "\"";
-    std::string result = Utilities::exec( stream.str().c_str() );
-    if( 0 == result.length() )
-      throw std::runtime_error( "Invalid response from Opal service" );
-    else if( !reader.parse( result.c_str(), root ) )
-      throw std::runtime_error( "Unable to parse result from Opal service" );
+
+    // encode the credentials
+    Utilities::base64String( this->Username + ":" + this->Password, credentials );
+    credentials = "Authorization:X-Opal-Auth " + credentials;
+
+    urlStream << "https://" << this->Host << ":" << this->Port << "/ws" + servicePath;
+    url = urlStream.str();
+
+    curl = curl_easy_init();
+    if( !curl ) 
+      throw std::runtime_error( "Unable to create cURL connection to Opal" );
+
+    // put the credentials in a header
+    headers = curl_slist_append( headers, credentials.c_str() );
+
+    // if we are writing to a file, open it
+    if( toFile ) file = fopen( fileName.c_str(), "wb" );
+
+    if( toFile ) curl_easy_setopt( curl, CURLOPT_WRITEFUNCTION, Utilities::writePointerToFile );
+    else curl_easy_setopt( curl, CURLOPT_WRITEFUNCTION, Utilities::writePointerToString );
+
+    if( toFile ) curl_easy_setopt( curl, CURLOPT_WRITEDATA, file );
+    else curl_easy_setopt( curl, CURLOPT_WRITEDATA, &result );
+
+    curl_easy_setopt( curl, CURLOPT_URL, url.c_str() );
+    curl_easy_setopt( curl, CURLOPT_SSL_VERIFYPEER, 0 );
+    curl_easy_setopt( curl, CURLOPT_HTTPHEADER, headers );
+    res = curl_easy_perform( curl );
+
+    // clean up
+    curl_slist_free_all( headers );
+    curl_easy_cleanup( curl );
+
+    if( 0 != res )
+    {
+      std::stringstream stream;
+      stream << "Received cURL error " << res << " when attempting to contact Opal";
+      throw std::runtime_error( stream.str().c_str() );
+    }
+
+    if( !toFile )
+    {
+      if( 0 == result.length() )
+        throw std::runtime_error( "Empty response from Opal service" );
+      else if( !reader.parse( result.c_str(), root ) )
+        throw std::runtime_error( "Unable to parse result from Opal service" );
+    }
 
     return root;
   }
